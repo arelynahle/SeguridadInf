f1340fb86da1eca0af5a76f6a27a8feaf3181567
RolandoAHT <RolandoAHT@192.168.1.73>
2014-11-30 12:10:36 -0600

    Institucion actualizacion

--- SuperCompuCampo/src/java/Model/Institucion.java
+++ SuperCompuCampo/src/java/Model/Institucion.java
@@ -9,30 +9,32 @@ public class Institucion {
     private int id_inst, numero_inst, cp_inst, max_ninos_inst;
     private String nombre_inst, calle_inst, colonia_inst, ciudad_inst, estado_inst, tel_inst, nombre_dir, ap_dir, am_dir, cel_dir, email_dir;
 
-    public Institucion(
-            String nombre_inst,
-            String calle_inst,
-            int numero_inst,
-            String colonia_inst,
-            int cp_inst,
-            String ciudad_inst,
-            String estado_inst,
-            String tel_inst,
-            int max_ninos_inst,
-            String nombre_dir,
-            String ap_dir,
-            String am_dir,
-            String cel_dir,
-            String email_dir
-    ) {
-
+    public Institucion(String nombre_inst, String calle_inst,int numero_inst,String colonia_inst,int cp_inst,String ciudad_inst,String estado_inst,String tel_inst,int max_ninos_inst,String nombre_dir,String ap_dir,String am_dir,String cel_dir,String email_dir) 
+    {
+        this.nombre_inst = nombre_inst;
+        this.calle_inst = calle_inst;
+        this.numero_inst = numero_inst;
+        this.colonia_inst = colonia_inst;
+        this.cp_inst = cp_inst;
+        this.ciudad_inst = ciudad_inst;
+        this.estado_inst = estado_inst;
+        this.tel_inst = tel_inst;
+        this.max_ninos_inst = max_ninos_inst;
+        this.nombre_dir = nombre_dir;
+        this.ap_dir = ap_dir;
+        this.am_dir = am_dir;
+        this.cel_dir = cel_dir;
+        this.email_dir = email_dir;
+    }
+        public Institucion(int id_inst, String nombre_inst, String calle_inst,int numero_inst,String colonia_inst,int cp_inst,String ciudad_inst,String estado_inst,String tel_inst,int max_ninos_inst,String nombre_dir,String ap_dir,String am_dir,String cel_dir,String email_dir) 
+    {
         this.id_inst = id_inst;
         this.nombre_inst = nombre_inst;
         this.calle_inst = calle_inst;
         this.numero_inst = numero_inst;
         this.colonia_inst = colonia_inst;
         this.cp_inst = cp_inst;
-        this.ciudad_inst=ciudad_inst;
+        this.ciudad_inst = ciudad_inst;
         this.estado_inst = estado_inst;
         this.tel_inst = tel_inst;
         this.max_ninos_inst = max_ninos_inst;
@@ -146,11 +148,11 @@ public class Institucion {
     public void setmax_ninos_inst(int max_ninos_inst) {
         this.max_ninos_inst = max_ninos_inst;
     }
-    
+
     public void setnombre_dir(String nombre_dir) {
         this.nombre_dir = nombre_dir;
     }
-           
+
     public void setap_dir(String ap_dir) {
         this.ap_dir = ap_dir;
     }
@@ -158,13 +160,13 @@ public class Institucion {
     public void setam_dir(String am_dir) {
         this.am_dir = am_dir;
     }
-    
+
     public void setcel_dir(String cel_dir) {
         this.cel_dir = cel_dir;
-    }    
+    }
 
     public void setemail_dir(String email_dir) {
         this.email_dir = email_dir;
-    }        
+    }
 
 }
